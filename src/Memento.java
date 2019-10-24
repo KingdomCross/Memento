717e7adbb55d606e7b2a88e29134d545e7fd3bbb
KingdomCross <alexchheng1998@yahoo.com>
2019-10-24 14:12:16 -0700

    re fix

--- src/Memento.java
+++ src/Memento.java
@@ -8,9 +8,10 @@
 public class Memento {
     public String flavor; //The available flavors are Chocolate, Vanilla, Strawberry and Mimi's Minty
     public String cone; //There is a choice of three cone types: regular, sugar or waffle
-    public String topping[]; //There is no limit to the number of toppings. You may use any toppings that you want.
-    public double totalPrice; //When the customer has made his final selection, display the cost of the cone. Create you own algorithm to calculate the cost.
+    public String[] topping; //There is no limit to the number of toppings. You may use any toppings that you want.
+    public int scoop; //This ice cream cone can have unlimited scoops of ice cream -- but only one flavor
 
     //The major difference here will be that the Memento objects themselves are being stored to a file. Do not create/store an ArrayList as shown in the examples. All of the Memento objects will be stored to the same file
     //Again, you are saving the state of the Ice Cream Cone to the Memento object, not the Ice Cream Cone itself.
+
 }
