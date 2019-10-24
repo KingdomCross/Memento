ceb12460f0c0ef7e2a1756340da269ad4edba802
KingdomCross <alexchheng1998@yahoo.com>
2019-10-24 14:16:10 -0700

    constructor getter toString

--- src/Memento.java
+++ src/Memento.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Alex Chheng and Chloe
  * 10/24/2019
@@ -6,12 +8,44 @@
  */
 //http://web.csulb.edu/~mopkins/cecs277/projectmemento.shtml
 public class Memento {
-    public String flavor; //The available flavors are Chocolate, Vanilla, Strawberry and Mimi's Minty
-    public String cone; //There is a choice of three cone types: regular, sugar or waffle
-    public String[] topping; //There is no limit to the number of toppings. You may use any toppings that you want.
-    public int scoop; //This ice cream cone can have unlimited scoops of ice cream -- but only one flavor
+    private String flavor; //The available flavors are Chocolate, Vanilla, Strawberry and Mimi's Minty
+    private String cone; //There is a choice of three cone types: regular, sugar or waffle
+    private String[] topping; //There is no limit to the number of toppings. You may use any toppings that you want.
+    private int scoop; //This ice cream cone can have unlimited scoops of ice cream -- but only one flavor
 
     //The major difference here will be that the Memento objects themselves are being stored to a file. Do not create/store an ArrayList as shown in the examples. All of the Memento objects will be stored to the same file
     //Again, you are saving the state of the Ice Cream Cone to the Memento object, not the Ice Cream Cone itself.
 
+    public Memento(String flavor, String cone, String[] topping, int scoop) {
+        this.flavor = flavor;
+        this.cone = cone;
+        this.topping = topping;
+        this.scoop = scoop;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public String getCone() {
+        return cone;
+    }
+
+    public String[] getTopping() {
+        return topping;
+    }
+
+    public int getScoop() {
+        return scoop;
+    }
+
+    @Override
+    public String toString() {
+        return "Memento{" +
+                "flavor='" + flavor + '\'' +
+                ", cone='" + cone + '\'' +
+                ", topping=" + Arrays.toString(topping) +
+                ", scoop=" + scoop +
+                '}';
+    }
 }
