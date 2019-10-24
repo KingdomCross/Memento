9d27ad3534821c6d53b88bcfa544f14fa1478679
KingdomCross <alexchheng1998@yahoo.com>
2019-10-24 14:21:05 -0700

    Recode to be similar to advance ice cream file

--- src/Memento.java
+++ src/Memento.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -8,44 +9,44 @@ import java.util.Arrays;
  */
 //http://web.csulb.edu/~mopkins/cecs277/projectmemento.shtml
 public class Memento {
+    private int numberOfScoops; //This ice cream cone can have unlimited scoops of ice cream -- but only one flavor
     private String flavor; //The available flavors are Chocolate, Vanilla, Strawberry and Mimi's Minty
-    private String cone; //There is a choice of three cone types: regular, sugar or waffle
-    private String[] topping; //There is no limit to the number of toppings. You may use any toppings that you want.
-    private int scoop; //This ice cream cone can have unlimited scoops of ice cream -- but only one flavor
+    private String typeOfCone; //There is a choice of three cone types: regular, sugar or waffle
+    private ArrayList<String> toppings = new ArrayList<String>(); //There is no limit to the number of toppings. You may use any toppings that you want.
 
     //The major difference here will be that the Memento objects themselves are being stored to a file. Do not create/store an ArrayList as shown in the examples. All of the Memento objects will be stored to the same file
     //Again, you are saving the state of the Ice Cream Cone to the Memento object, not the Ice Cream Cone itself.
 
-    public Memento(String flavor, String cone, String[] topping, int scoop) {
+    public Memento(int numberOfScoops, String flavor, String typeOfCone, ArrayList<String> toppings) {
+        this.numberOfScoops = numberOfScoops;
         this.flavor = flavor;
-        this.cone = cone;
-        this.topping = topping;
-        this.scoop = scoop;
+        this.typeOfCone = typeOfCone;
+        this.toppings = toppings;
+    }
+
+    public int getNumberOfScoops() {
+        return numberOfScoops;
     }
 
     public String getFlavor() {
         return flavor;
     }
 
-    public String getCone() {
-        return cone;
+    public String getTypeOfCone() {
+        return typeOfCone;
     }
 
-    public String[] getTopping() {
-        return topping;
-    }
-
-    public int getScoop() {
-        return scoop;
+    public ArrayList<String> getToppings() {
+        return toppings;
     }
 
     @Override
     public String toString() {
         return "Memento{" +
-                "flavor='" + flavor + '\'' +
-                ", cone='" + cone + '\'' +
-                ", topping=" + Arrays.toString(topping) +
-                ", scoop=" + scoop +
+                "numberOfScoops=" + numberOfScoops +
+                ", flavor='" + flavor + '\'' +
+                ", typeOfCone='" + typeOfCone + '\'' +
+                ", toppings=" + toppings +
                 '}';
     }
 }
