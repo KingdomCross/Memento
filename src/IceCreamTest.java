644016627c9d2cc891bf0f434961e61a03c61356
KingdomCross <alexchheng1998@yahoo.com>
2019-10-24 14:46:42 -0700

    Further testing now awaiting reupload from mimi

--- src/IceCreamTest.java
+++ src/IceCreamTest.java
@@ -2,5 +2,6 @@ public class IceCreamTest {
     public static void main(String [] args) {
         AdvancedIceCreamCone iceCreamCone = new AdvancedIceCreamCone(2, "Vanilla", "Waffle");
         CareTaker careTaker = new CareTaker();
+        System.out.println("original state 0: " + iceCreamCone);
     }
 }
